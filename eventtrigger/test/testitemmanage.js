c7cce094461fac8f9494aaa470270f144fbbba31
luigicallero <luigicallero@gmail.com>
2022-04-04 08:11:36 -0300

    last udpates

--- eventtrigger/test/testitemmanage.js
+++ eventtrigger/test/testitemmanage.js
@@ -7,6 +7,9 @@ contract("ItemManager", accounts => {
     const itemPrice = 500;
 
     const result = await itemManagerInstance.createItem(itemName, itemPrice, {from: accounts[0]});
-    console.log(result);
+    assert.equal(result.logs[0].args._itemIndex, 0, "It is not the first item");
+
+    const item = await itemManagerInstance.items(0);
+    assert.equal(item._identifier, itemName, "The identifier was different");
   })
 });
