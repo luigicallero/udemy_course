3c13eb9d34ec007a977a1d11ec197bad1ec587ef
luigicallero <luigicallero@gmail.com>
2022-04-21 09:06:23 -0300

    tests to Token Sale completed

--- myToken/test/MyTokenSale.test.js
+++ myToken/test/MyTokenSale.test.js
@@ -16,5 +16,18 @@ contract("Testing Token Sale", async(accounts) => {
     return expect(balance).to.be.a.bignumber.equal(new BN(0));
   })
 
+  it("all tokens should be at Token Sale Contract by default", async() => {
+    let instance = await Token.deployed();
+    let tokenSalebalance = await instance.balanceOf(TokenSale.address);
+    let totalSupply = await instance.totalSupply();
+    return expect(tokenSalebalance).to.be.a.bignumber.equal(totalSupply);
+  })
 
+  it("should be possible to buy tokens from Token Sale Contract", async() => {
+    let tokenInstance = await Token.deployed();
+    let tokenSaleInstance = await TokenSale.deployed();
+    let balanceBefore = await tokenInstance.balanceOf(deployerAccount);
+    await expect(tokenSaleInstance.sendTransaction({from: deployerAccount, value:web3.utils.toWei('1','wei')})).to.be.fulfilled; // buying 1 token equal 1 wei
+    await expect(tokenInstance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.equal(balanceBefore + new BN(1));
+  })
 })
