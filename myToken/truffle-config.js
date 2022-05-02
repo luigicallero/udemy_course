7c656a00b6c94d9d85d7fb473b3227e397530a30
luigicallero <luigicallero@gmail.com>
2022-05-02 09:01:41 -0300

    HDwallet added

--- myToken/truffle-config.js
+++ myToken/truffle-config.js
@@ -1,4 +1,8 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
 const path = require("path");
+const AccountIndex = 0;
+
+require("dotenv").config({path: "./.env"});
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
@@ -6,8 +10,32 @@ module.exports = {
   contracts_build_directory: path.join(__dirname, "client/src/contracts"),
   networks: {
     develop: {
-      port: 8545
-    }
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*", // match any network
+      websockets: true
+    },
+    ganache: {
+      provider: function() {
+        return new HDWalletProvider(process.env.MNEMONIC, "http://127.0.0.1:8545/", AccountIndex);
+      },
+      network_id: '1651490686169',
+    },
+    truffle: {
+      // must be a thunk, otherwise truffle commands may hang in CI
+      provider: () =>
+        new HDWalletProvider({
+          mnemonic: {
+            phrase: process.env.MNEMONIC
+          },
+          providerOrUrl: "http://127.0.0.1:8545",
+        }),
+      network_id: '5777',
+    },
+    matic: {
+      provider: () => new HDWalletProvider(process.env.MNEMONIC, `http://127.0.0.1:8545`),
+      network_id: "*"
+    },
   },
   compilers: {
     solc: {
